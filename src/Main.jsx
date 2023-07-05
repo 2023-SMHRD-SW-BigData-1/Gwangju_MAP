2cb0c370c1f6acee2c4fdd78d9386bbf2c133c0c
HakSeong <harryjin12@naver.com>
2023-07-05 14:05:25 +0900

    first

---
+++ src/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Main.css'
+import Header from './Header'
+
+
+const Main = () => {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default Main
