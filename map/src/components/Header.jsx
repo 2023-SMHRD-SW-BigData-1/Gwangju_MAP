0d240424ca1ba46b21b08e4fdafb6b96768bf2e8
HakSeong <harryjin12@naver.com>
2023-07-10 20:04:24 +0900

    123

---
+++ map/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import firstChart from './chart/firstChart'
+
+
+const Header = () => {
+  return (
+   <div></div>
+  )
+}
+
+export default Header
---
+++ map/src/components/Main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './Header'
+
+const Main = () => {
+  return (
+    <div className='main' style={{backgroundColor : '#F4FFFF'}}>
+      <Header/>
+        <div className = "FCT" >
+          <div className='FCT1' style={{ width : 1000, height : 600}}>
+            <div className='FCT1T'>
+          <h3>광주5대 범죄 발생건수 그래프</h3>
+            </div>
+            < firstChart/></div>
+            </div>
+
+
+
+
+
+            </div>
+  )
+}
+
+export default Main
