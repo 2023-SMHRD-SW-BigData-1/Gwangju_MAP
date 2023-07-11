89aeb8c5f70f69316f65898e942bb0564b185a7c
HakSeong <harryjin12@naver.com>
2023-07-11 10:35:08 +0900

    server-err

--- map/server.js
+++ map/server.js
@@ -1,11 +1,17 @@
-const express = require('express');
+const express = require('express')
 const app = express()
 const path = require('path')
+const indexRouter = require('./src/routes')
 
-app.set('port',process.env.PORT || 8080)
 
+app.set('port',process.env.PORT || 3000)
 
-app.listen(app.get('port'), ()=> {
-    console.log('8090서버 접속 중');
+app.use(express.json())
+app.use(cors())
 
+app.use(express.static(path.join(__dirname, "map/build/index.html")))
+
+app.use('/', indexRouter)    
+app.listen(app.get('port'), ()=>{
+    console.log(app.get('port'), '번 port에서 서버연결 대기중..')
 })
