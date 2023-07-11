4c39e9ecb7318ba7b1d7b8610748f0274c4547b3
HakSeong <harryjin12@naver.com>
2023-07-11 13:55:10 +0900

    Loginin

--- map/src/pages/Login.jsx
+++ map/src/pages/Login.jsx
@@ -1,21 +1,35 @@
-import React,{} from 'react'
+import React,{ useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 const Login = () => {
 
   const result = axios.post('http://localhost:8888/user/login',{mb_id: 'mb_id',mb_pw: 'mb_pw'})
+ 
 
+  const [mb_id, setMb_Id] = useState("");
+  const [mb_pw, setMb_Pw] = useState("");
+
+const handleinputid = (e) =>{
+  setMb_Id(e.target.value)
+}
   
+const handleinputpw = (e) =>{
+  setMb_Pw(e.target.value)
+}
   
   
   return (
     <div>
-        <input type="text" name='id'/>
-        <input type="password" name='pw'/>
-        <input type="submit" value="제출"/>
-
+      <h1>안전지도 회원가입</h1>
+      <label>아이디 입력</label>
+        <input type="text" name='id' value='mb_id' onChange/><br/>
+      <label>비밀번호 입력</label>
+        <input type="password" name='pw' value='mb_pw' />
+        <button type="submit">로그인</button>
         <Link to='pages/Join'><h3>회원가입</h3></Link>
+
+
     </div>
   )
 }
