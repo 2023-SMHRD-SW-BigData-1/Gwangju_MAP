5082e8304b4c6ff3549c1267e88ac921cf04fb49
HakSeong <harryjin12@naver.com>
2023-07-17 08:59:53 +0900

    Logout

--- map/src/components/Header.jsx
+++ map/src/components/Header.jsx
@@ -1,30 +1,34 @@
-import React, { useState } from 'react';
-import './Header.css';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  
-  
+  const [isLoggedIn, setIsLoggedIn] = useState(sessionStorage.getItem('nick') !== null);
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('nick');
+    setIsLoggedIn(false);
+  };
 
   return (
     <div>
-      
-      
       <div className="header">
         <div className="tlt1">
           <h1>광주안전지도</h1>
         </div>
         <div className="login">
-          <Link to="/pages/Login">
-          <button type="button" className="btn btn-primary btn-lg">로그인</button>
-          </Link>
+          {isLoggedIn ? (
+            <button type="button" className="btn btn-primary btn-lg" onClick={handleLogout}>
+              로그아웃
+            </button>
+          ) : (
+            <Link to="/pages/Login">
+              <button type="button" className="btn btn-primary btn-lg">
+                로그인
+              </button>
+            </Link>
+          )}
         </div>
       </div>
-
-
     </div>
   );
 };
