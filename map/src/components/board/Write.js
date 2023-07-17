27e94446f26d6acc58144085b0b10c0bc5e38f76
usernamekimdaeun <daeun1505@naver.com>
2023-07-17 11:51:55 +0900

    ㅁㄴㅇㄹ

--- map/src/components/board/Write.js
+++ map/src/components/board/Write.js
@@ -30,19 +30,6 @@ class Write extends Component {
             });
     };
 
-    update = () => {
-        Axios.post("http://localhost:8888/b_update", {
-            title: this.state.title,
-            region: this.state.region,
-            content: this.state.content,
-        })
-            .then((res) => {
-                console.log(res);
-            })
-            .catch((e) => {
-                console.error(e);
-            });
-    };
 
     handleChange = (e) => {
         this.setState({
